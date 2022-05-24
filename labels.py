a7e4db50abe12051e9a5354ad0f2459623092b91
maxgao18 <maxgao18@yahoo.com>
2022-05-24 15:54:28 -0400

    grind

--- labels.py
+++ labels.py
@@ -2,14 +2,28 @@ import pandas as pd
 import numpy as np
 
 
-def get_concurrent_label_count(data: pd.DataFrame, barriers: pd.DataFrame):
+def _effective_barrier_end(barriers: pd.DataFrame, last_date):
     times = barriers[["pt_dt", "sl_dt"]].copy()
     times["end"] = barriers["vb"].fillna(pd.NaT) + times.index
-    times["max"] = data.index[-1]
+    times["max"] = last_date
     times = times.min(axis=1)
 
+    return times
+
+
+def get_concurrent_label_count(data: pd.DataFrame, barriers: pd.DataFrame):
+    times = _effective_barrier_end(barriers, data.index[-1])
     count = pd.Series(0, index=data.index)
     for s, e in times.iteritems():
         count[s:e] += 1
 
     return count
+
+
+def get_label_uniqueness(barriers: pd.DataFrame, concurrent_label_count: pd.Series):
+    times = _effective_barrier_end(barriers, concurrent_label_count.index[-1])
+    uniqueness = pd.Series(0, index=barriers.index)
+    for s, e in times.iteritems():
+        uniqueness.loc[s] = (1.0 / concurrent_label_count.loc[s:e]).mean()
+
+    return uniqueness
