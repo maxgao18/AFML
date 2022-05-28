a11c8aa2e3531cb6f6b38befc1de9219218254f6
maxgao18 <maxgao18@yahoo.com>
2022-05-28 18:43:26 -0400

    in progress

--- portfolio.py
+++ portfolio.py
@@ -22,19 +22,21 @@ def ivp_allocation(covariance_matrix: pd.DataFrame):
 def hrp_allocation(covariance_matrix: pd.DataFrame):
     if len(covariance_matrix.index) <= 2:
         return ivp_allocation(covariance_matrix)
-    
+
     index = int(len(covariance_matrix.index) / 2)
     left_cluster = covariance_matrix.index[:index]
-    left_cluster = covariance_matrix.loc[left_cluster,left_cluster]
+    left_cluster = covariance_matrix.loc[left_cluster, left_cluster]
     right_cluster = covariance_matrix.index[index:]
-    right_cluster = covariance_matrix.loc[right_cluster,right_cluster]
+    right_cluster = covariance_matrix.loc[right_cluster, right_cluster]
 
     left_allocation = hrp_allocation(left_cluster)
     right_allocation = hrp_allocation(right_cluster)
 
     left_variance = variance(ivp_allocation(left_cluster), left_cluster)
     right_variance = variance(ivp_allocation(right_cluster), right_cluster)
-    
+
     left_weight = right_variance / (left_variance + right_variance)
-    
-    return pd.concat([left_weight * left_allocation, (1-left_weight) * right_allocation])
+
+    return pd.concat(
+        [left_weight * left_allocation, (1 - left_weight) * right_allocation]
+    )
