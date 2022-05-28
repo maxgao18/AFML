c4134d14c277f4974960db268d6470c13f011983
maxgao18 <maxgao18@yahoo.com>
2022-05-28 18:43:01 -0400

    in progress

---
+++ portfolio.py
@@ -0,0 +1,40 @@
+import numpy as np
+import pandas as pd
+import scipy.cluster.hierarchy as sch
+
+
+def variance(allocations, covariance_matrix):
+    return np.dot(np.matmul(covariance_matrix, allocations), allocations)
+
+
+def quasi_diagonalize(link, matrix: pd.DataFrame):
+    leaves = sch.leaves_list(link)
+    diagonal = [matrix.index[l] for l in leaves]
+    return matrix.reindex(diagonal)[diagonal]
+
+
+def ivp_allocation(covariance_matrix: pd.DataFrame):
+    allocations = 1 / np.diag(covariance_matrix)
+    allocations /= allocations.sum()
+    return pd.Series(allocations, index=covariance_matrix.index)
+
+
+def hrp_allocation(covariance_matrix: pd.DataFrame):
+    if len(covariance_matrix.index) <= 2:
+        return ivp_allocation(covariance_matrix)
+    
+    index = int(len(covariance_matrix.index) / 2)
+    left_cluster = covariance_matrix.index[:index]
+    left_cluster = covariance_matrix.loc[left_cluster,left_cluster]
+    right_cluster = covariance_matrix.index[index:]
+    right_cluster = covariance_matrix.loc[right_cluster,right_cluster]
+
+    left_allocation = hrp_allocation(left_cluster)
+    right_allocation = hrp_allocation(right_cluster)
+
+    left_variance = variance(ivp_allocation(left_cluster), left_cluster)
+    right_variance = variance(ivp_allocation(right_cluster), right_cluster)
+    
+    left_weight = right_variance / (left_variance + right_variance)
+    
+    return pd.concat([left_weight * left_allocation, (1-left_weight) * right_allocation])
